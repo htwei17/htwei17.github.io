ddd00e02ecb923e1f9095c88cf67efc168a8230d
htwei17 <weihaotian776@gmail.com>
2023-10-21 00:59:47 -0500

    Initial commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "My First Post",
+        "excerpt":"This is my first post. I am excited to share my research and insights here.  ","categories": ["update"],
+        "tags": [],
+        "url": "/update/2023/10/20/note.html",
+        "teaser": "/assets/images/fanjingshan.jpeg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2023/10/20/welcome-to-jekyll.html",
+        "teaser": "/assets/images/fanjingshan.jpeg"
+      }]
