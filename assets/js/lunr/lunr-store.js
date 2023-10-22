b44a40fa3c307d987c63d8bf44ee056f6a40bd3b
htwei17 <htwei17@users.noreply.github.com>
2023-10-22 18:28:13 +0000

    deploy: ec2617a76ca98c24c4739661903496234fd69283

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,10 +4,4 @@ var store = [{
         "tags": [],
         "url": "/update/2023/10/20/note.html",
         "teaser": "/assets/images/fanjingshan.jpeg"
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2023/10/21/welcome-to-jekyll.html",
-        "teaser": "/assets/images/fanjingshan.jpeg"
       }]
