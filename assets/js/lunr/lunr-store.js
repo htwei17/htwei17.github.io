b5b6f102f1114db5cf6ea577477ef139e5c98956
htwei17 <htwei17@users.noreply.github.com>
2023-10-29 03:15:01 +0000

    deploy: fe97c0678bdccf2a62d376842a9ffa16c72a9096

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "My First Post",
-        "excerpt":"This is my first post. I am excited to share my research and insights here.  ","categories": ["update"],
+        "title": "My Logo",
+        "excerpt":"The site logo is:      The logo is a combination of the following factors:      A clock to tick the proper time   A $\\psi$ symbol to represent the quantum state  ","categories": ["update"],
         "tags": [],
         "url": "/update/2023/10/20/note.html",
         "teaser": "/assets/images/fanjingshan.jpeg"
