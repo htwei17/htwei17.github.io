133db19122bfcc734c945f4d87c1186cd0db85c5
htwei17 <htwei17@users.noreply.github.com>
2024-01-16 01:19:37 +0000

    deploy: 9c05b0f6df95652b6e7e96ced8acd260b7b4d0f5

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,12 @@ var store = [{
         "title": "My Logo",
         "excerpt":"The site logo is:      The logo is a combination of the following factors:      A clock to tick the proper time   A $\\psi$ symbol to represent the quantum state  ","categories": ["update"],
         "tags": [],
-        "url": "/update/2023/10/20/note.html",
+        "url": "/update/2023/10/20/logo.html",
+        "teaser": "/assets/images/fanjingshan.jpeg"
+      },{
+        "title": "Quantum Resource Theory",
+        "excerpt":" ","categories": ["note"],
+        "tags": [],
+        "url": "/note/2024/01/15/qrt.html",
         "teaser": "/assets/images/fanjingshan.jpeg"
       }]
