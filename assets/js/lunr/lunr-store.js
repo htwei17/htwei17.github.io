39d68ea040091486b1ba1cbc30088a36714b2fde
htwei17 <htwei17@users.noreply.github.com>
2024-01-18 02:18:38 +0000

    deploy: aebef1611b11fde43443b5e24f0e3cf5a42d6b64

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,9 +5,15 @@ var store = [{
         "url": "/update/2023/10/20/logo.html",
         "teaser": "/assets/images/fanjingshan.jpeg"
       },{
-        "title": "Quantum Resource Theory",
-        "excerpt":" ","categories": ["note"],
+        "title": "Quantum Resource Theory by the Example of Quantum Entanglement",
+        "excerpt":"Quantum Resource Theory as a Framework for Quantum Information Processing Quantum Entanglement in the Context of Quantum Resource Theory In this part we illustrate how concepts of quantum resource theory can be applied to the study of quantum entanglement. We will first introduce the concept of local operations and classical...","categories": ["note"],
         "tags": [],
         "url": "/note/2024/01/15/qrt.html",
+        "teaser": "/assets/images/fanjingshan.jpeg"
+      },{
+        "title": "Blitz Note on Quantum Measurement",
+        "excerpt":"The quantum measurement, formally acting on a density matrix $\\rho$ as \\(\\rho \\rightarrow \\sum_{i} M_{i} \\rho M_{i}^{\\dagger}.\\) is a special case of quantum operation, where $i$ indices the different outcomes . The measurement Kraus operators $M_{i}$ are completely positive and satisfy condition s.t. the trace of the density matrix is...","categories": ["note"],
+        "tags": [],
+        "url": "/note/2024/01/16/povm.html",
         "teaser": "/assets/images/fanjingshan.jpeg"
       }]
